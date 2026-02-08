2e81ce6fee6f74c8edfdaa6dade500a69e6551fe
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2026-02-08 19:27:04 +0700

    fix(next.config): Fix next config env

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_FIVEM_ASSETS_URL: process.env.FIVEM_ASSETS_URL,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.pinimg.com", pathname: "/**" },
@@ -12,11 +9,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "ui-avatars.com", pathname: "/api/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "cdn.discordapp.com", pathname: "/**" },
-      {
-        protocol: "https",
-        hostname: "assets.gclindonesia.com",
-        pathname: "/**",
-      },
       {
         protocol: "http",
         hostname: "assets.gclindonesia.com",
